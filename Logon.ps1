bd92a74cf8477ee65bbd47646118b87c4c9c8bd6
Robert Tingirica <rtingirica@cloudbasesolutions.com>
2014-07-28 16:24:14 +0300

    added winrm configuration

--- Logon.ps1
+++ Logon.ps1
@@ -2,105 +2,22 @@ $ErrorActionPreference = "Stop"
 
 try
 {
-     # $Host.UI.RawUI.WindowTitle = "Downloading PSWindowsUpdate..."
+    $Host.UI.RawUI.WindowTitle = "Winrm config..."
+    $winrmLUrl = "https://raw.githubusercontent.com/trobert2/unattended-setup-scripts/master/SetupWinRMAccess.ps1"
+    $winrmPath = "$ENV:Temp\SetupWinRMAccess.ps1"
+    (new-object System.Net.WebClient).DownloadFile($winrmLUrl, $winrmPath)
+    powershell -NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -File $winrmPath
+    winrm set winrm/config/service `@`{AllowUnencrypted=`"true`"`}
 
-     # $psWindowsUpdateBaseUrl = "http://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc/file/"
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name Unattend*
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
 
-     # #Fixes Windows Server 2008 R2 inexistent Unblock-File command Bug
-     # if ($(Get-Host).version.major -eq 2)
-     # {
-     #     $psWindowsUpdateUrl = $psWindowsUpdateBaseUrl + "66095/1/PSWindowsUpdate_1.4.5.zip"
-     # }
-     # else
-     # {
-     #     $psWindowsUpdateUrl = $psWindowsUpdateBaseUrl + "41459/25/PSWindowsUpdate.zip"
-     # }
-
-     # $psWindowsUpdatePath = "$ENV:Temp\PSWindowsUpdate.zip"
-     # (new-object System.Net.WebClient).DownloadFile($psWindowsUpdateUrl, $psWindowsUpdatePath)
-
-     # $Host.UI.RawUI.WindowTitle = "Installing PSWindowsUpdate..."
-     # foreach($item in (New-Object -com shell.application).NameSpace($psWindowsUpdatePath).Items())
-     # {
-     #     $yesToAll = 16
-     #     (New-Object -com shell.application).NameSpace("$ENV:SystemRoot\System32\WindowsPowerShell\v1.0\Modules").copyhere($item, $yesToAll)
-     # }
-     # del $psWindowsUpdatePath
-
-     # Import-Module PSWindowsUpdate
-
-     # $Host.UI.RawUI.WindowTitle = "Installing updates..."
-
-     # Get-WUInstall -AcceptAll -IgnoreReboot -IgnoreUserInput -NotCategory "Language packs"
-
-     # if (Get-WURebootStatus -Silent)
-     # {
-     #     $Host.UI.RawUI.WindowTitle = "Updates installation finished. Rebooting."
-     #     shutdown /r /t 0
-     # }
-     # else
-     # {
-     #   $Host.UI.RawUI.WindowTitle = "Downloading Cloudbase-Init..."
-
-     #   $osArch = (Get-WmiObject  Win32_OperatingSystem).OSArchitecture
-     #   if($osArch -eq "64-bit")
-     #   {
-     #       $CloudbaseInitMsi = "CloudbaseInitSetup_Beta_x64.msi"
-     #       $programFilesDir = ${ENV:ProgramFiles(x86)}
-     #   }
-     #   else
-     #   {
-     #       $CloudbaseInitMsi = "CloudbaseInitSetup_Beta_x86.msi"
-     #       $programFilesDir = $ENV:ProgramFiles
-     #   }
-
-     #   $CloudbaseInitMsiPath = "$ENV:Temp\$CloudbaseInitMsi"
-     #   $CloudbaseInitMsiUrl = "http://www.cloudbase.it/downloads/$CloudbaseInitMsi"
-     #   $CloudbaseInitMsiLog = "$ENV:Temp\CloudbaseInitSetup_Beta.log"
-
-     #   (new-object System.Net.WebClient).DownloadFile($CloudbaseInitMsiUrl, $CloudbaseInitMsiPath)
-
-     #   $Host.UI.RawUI.WindowTitle = "Installing Cloudbase-Init..."
-
-     #   $serialPortName = @(Get-WmiObject Win32_SerialPort)[0].DeviceId
-
-     #   $p = Start-Process -Wait -PassThru -FilePath msiexec -ArgumentList "/i $CloudbaseInitMsiPath /qn /l*v $CloudbaseInitMsiLog LOGGINGSERIALPORTNAME=$serialPortName"
-     #   if ($p.ExitCode -ne 0)
-     #   {
-     #       throw "Installing $CloudbaseInitMsiPath failed. Log: $CloudbaseInitMsiLog"
-     #   }
-
-     #   #replace git code with newest code
-     #   $CloudbaseInitInstalationFolder = "$programFilesDir\Cloudbase Solutions\Cloudbase-Init\Python27\Lib\site-packages\cloudbaseinit"
-     #   $CloneDir = "$env:TMP\cloudbase"
-     #   Remove-Item -Force -Recurse $CloudbaseInitInstalationFolder
-
-        # git clone "https://github.com/trobert2/cloudbase-init.git" $CloneDir
-        # cd $CloneDir
-        # git checkout nits_pep8
-        # cd ~
-        # Move-Item -Force A:\\cloudbaseinit $CloudbaseInitInstalationFolder
-
-        $Host.UI.RawUI.WindowTitle = "Adding rules for winrm..."
-        New-NetFirewallRule -DisplayName "Allow winrm http" -Direction Inbound -LocalPort 5985 -Protocol TCP -Action Allow
-        New-NetFirewallRule -DisplayName "Allow winrm https" -Direction Inbound -LocalPort 5986 -Protocol TCP -Action Allow
-        New-NetFirewallRule -DisplayName "Allow winrm http" -Direction Outbound -LocalPort 5985 -Protocol TCP -Action Allow
-        New-NetFirewallRule -DisplayName "Allow winrm https" -Direction Outbound -LocalPort 5986 -Protocol TCP -Action Allow
-         # We're done, remove LogonScript and disable AutoLogon
-
-        Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name Unattend*
-        Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
-
-        # $Host.UI.RawUI.WindowTitle = "Running SetSetupComplete..."
-        # & "$programFilesDir\Cloudbase Solutions\Cloudbase-Init\bin\SetSetupComplete.cmd"
-
-        $Host.UI.RawUI.WindowTitle = "Running Sysprep..."        
-        $unattendXMLUrl = "https://raw.githubusercontent.com/trobert2/windows-openstack-imaging-tools/master/Unattend.xml"
-        $unattendXMLPath = "$ENV:Temp\Unattend.xml"
-        (new-object System.Net.WebClient).DownloadFile($unattendXMLUrl, $unattendXMLPath)
-        & "$ENV:SystemRoot\System32\Sysprep\Sysprep.exe" `/generalize `/oobe `/shutdown `/unattend:"$unattendXMLPath"
-    }
-# }
+    $Host.UI.RawUI.WindowTitle = "Running Sysprep..."        
+    $unattendXMLUrl = "https://raw.githubusercontent.com/trobert2/windows-openstack-imaging-tools/master/Unattend.xml"
+    $unattendXMLPath = "$ENV:Temp\Unattend.xml"
+    (new-object System.Net.WebClient).DownloadFile($unattendXMLUrl, $unattendXMLPath)
+    & "$ENV:SystemRoot\System32\Sysprep\Sysprep.exe" `/generalize `/oobe `/shutdown `/unattend:"$unattendXMLPath"
+}   
 catch
 {
     $host.ui.WriteErrorLine($_.Exception.ToString())
